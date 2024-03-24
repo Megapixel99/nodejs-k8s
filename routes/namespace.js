34a0a094f21261b6f020c8a74e63aeba13ce9e37
Seth Wheeler <23089578+Megapixel99@users.noreply.github.com>
2024-03-24 12:56:22 -0500

    update namespace

--- routes/namespace.js
+++ routes/namespace.js
@@ -1,30 +1,52 @@
 const router = require('express').Router();
 const { parse, stringify } = require('yaml');
 const { Namespace } = require('../database/models.js');
+const { duration } = require('../functions.js');
 
 router.get('/', (req, res, next) => {
-  namespace.find().then((namespaces) => {
+  Namespace.find().then((namespaces) => {
     res.send({
-      items: namespaces,
-      metadata: {
-        remainingItemCount: 0,
-        continue: false,
-        resourceVersion: "v1",
-      },
-      apiVersion: "v1",
-      kind: "NamespaceList",
+      "kind": "Table",
+      "apiVersion": "meta.k8s.io/v1",
+      "columnDefinitions": [
+        {
+          "name": "Name",
+          "type": "string",
+          "format": "name",
+          "description": "Name must be unique within a namespace. Is required when creating resources, although some resources may allow a client to request the generation of an appropriate name automatically. Name is primarily intended for creation idempotence and configuration definition. Cannot be updated. More info: http://kubernetes.io/docs/user-guide/identifiers#names",
+          "priority": 0
+        },
+        {
+          "name": "Age",
+          "type": "string",
+          "format": "",
+          "description": "CreationTimestamp is a timestamp representing the server time when this object was created. It is not guaranteed to be set in happens-before order across separate operations. Clients may not set this value. It is represented in RFC3339 form and is in UTC.\n\nPopulated by the system. Read-only. Null for lists. More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata",
+          "priority": 0
+        },
+      ],
+      "rows": namespaces.map((e) => ({
+        "cells": [
+          e.metadata.name,
+          duration(new Date() - e.metadata.creationTimestamp),
+        ],
+        object: {
+          "kind": "PartialObjectMetadata",
+          "apiVersion": "meta.k8s.io/v1",
+          metadata: e.metadata,
+        }
+      })),
     });
   }).catch(next);
 });
 
 router.get('/:name', (req, res, next) => {
-  namespace.find({ metadata: { name: req.query.name } }).then((namespace) => {
+  Namespace.find({ metadata: { name: req.query.name } }).then((namespace) => {
     res.send(namespace);
   }).catch(next);
 });
 
 router.get('/:name/status', (req, res, next) => {
-  namespace.find({ metadata: { name: req.query.name } }).then((namespace) => {
+  Namespace.find({ metadata: { name: req.query.name } }).then((namespace) => {
     res.send(namespace.status);
   }).catch(next);
 });
