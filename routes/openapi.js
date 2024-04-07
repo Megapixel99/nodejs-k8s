84a3e63ac19d8afb8fb678144da73fab5d530bff
Seth Wheeler <23089578+Megapixel99@users.noreply.github.com>
2024-04-07 02:11:01 -0500

    update routes for networking

--- routes/openapi.js
+++ routes/openapi.js
@@ -23,7 +23,7 @@ const apiAppsV1OpenApiV3 = ApiAppsV1OpenApiV3(apiAppsV1OpenApiV3Spec);
 
 router.get('/openapi/v3', (req, res) => res.json(openapiv3.document));
 router.get('/openapi/v3/apis/apps/v1', (req, res) => res.json(apiAppsV1OpenApiV3.document));
-router.get('/openapi/v3/apis/networking.k8s.io/v1', (req, res) => res.json(apiNetworkingK8sIoV1OpenApiV3.document));
+router.get(['/openapi/v3/apis/networking.k8s.io/v1', '/apis/networking.k8s.io/v1'], (req, res) => res.json(apiNetworkingK8sIoV1OpenApiV3.document));
 router.get('/openapi/v3/api/v1', (req, res) => res.json(apiV1OpenapiV3.document));
 
 module.exports = {
