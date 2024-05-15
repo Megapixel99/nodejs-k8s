ede15d0e526009c012dc2541b81abbc0bf9ededd
Seth Wheeler <23089578+Megapixel99@users.noreply.github.com>
2024-05-15 02:14:32 -0500

    update namespace deletion

--- objects/namespace.js
+++ objects/namespace.js
@@ -1,14 +1,104 @@
 const { DateTime } = require('luxon');
+const selfsigned = require('selfsigned');
 const K8Object = require('./object.js');
 const { Namespace: Model } = require('../database/models.js');
 const Models = require('../database/models.js');
-const Pod = require('./pod.js');
+const CertificateSigningRequest = require('./certificateSigningRequest.js');
+const ClusterRole = require('./clusterRole.js');
+const ClusterRoleBinding = require('./clusterRoleBinding.js');
+const ConfigMap = require('./configMap.js');
+const ControllerRevision = require('./controllerRevision.js');
+const CronJob = require('./cronJob.js');
+const Csidriver = require('./csidriver.js');
+const CsiNode = require('./csiNode.js');
+const CsiStorageCapacity = require('./csiStorageCapacity.js');
+const Daemonset = require('./daemonset.js');
+const Deployment = require('./deployment.js');
 const Endpoints = require('./endpoints.js');
+const EndpointSlice = require('./endpointSlice.js');
+const Event = require('./event.js');
+const HorizontalPodAutoscaler = require('./horizontalPodAutoscaler.js');
+const IngressClass = require('./ingressClass.js');
+const Job = require('./job.js');
+const LimitRange = require('./limitRange.js');
+const LocalSubjectAccessReview = require('./localSubjectAccessReview.js');
+const MutatingWebhookConfiguration = require('./mutatingWebhookConfiguration.js');
+const NetworkPolicy = require('./networkPolicy.js');
+const PersistentVolume = require('./persistentVolume.js');
+const PersistentVolumeClaim = require('./persistentVolumeClaim.js');
+const Pod = require('./pod.js');
+const PodDisruptionBudget = require('./podDisruptionBudget.js');
+const PodTemplate = require('./podTemplate.js');
+const PriorityClass = require('./priorityClass.js');
+const Replicaset = require('./replicaset.js');
+const ReplicationController = require('./replicationController.js');
+const ResourceQuota = require('./resourceQuota.js');
+const Role = require('./role.js');
+const RoleBinding = require('./roleBinding.js');
+const Secret = require('./secret.js');
+const SelfSubjectReview = require('./selfSubjectReview.js');
+const SelfSubjectAccessReview = require('./selfSubjectAccessReview.js');
+const SelfSubjectRulesReview = require('./selfSubjectRulesReview.js');
+const Service = require('./service.js');
+const ServiceAccount = require('./serviceAccount.js');
+const StatefulSet = require('./statefulSet.js');
+const StorageClass = require('./storageClass.js');
+const SubjectAccessReview = require('./subjectAccessReview.js');
+const TokenRequest = require('./tokenRequest.js');
+const TokenReview = require('./tokenReview.js');
+const ValidatingWebhookConfiguration = require('./validatingWebhookConfiguration.js');
+const VolumeAttachment = require('./volumeAttachment.js');
 const { duration } = require('../functions.js');
 
+const objects = {
+  CertificateSigningRequest,
+  ClusterRole,
+  ClusterRoleBinding,
+  ConfigMap,
+  ControllerRevision,
+  CronJob,
+  Csidriver,
+  CsiNode,
+  CsiStorageCapacity,
+  Daemonset,
+  Deployment,
+  Endpoints,
+  EndpointSlice,
+  Event,
+  HorizontalPodAutoscaler,
+  IngressClass,
+  Job,
+  LimitRange,
+  LocalSubjectAccessReview,
+  MutatingWebhookConfiguration,
+  NetworkPolicy,
+  PersistentVolume,
+  PersistentVolumeClaim,
+  Pod,
+  PodDisruptionBudget,
+  PodTemplate,
+  PriorityClass,
+  Replicaset,
+  ReplicationController,
+  ResourceQuota,
+  Role,
+  RoleBinding,
+  Secret,
+  SelfSubjectReview,
+  SelfSubjectAccessReview,
+  SelfSubjectRulesReview,
+  Service,
+  ServiceAccount,
+  StatefulSet,
+  StorageClass,
+  SubjectAccessReview,
+  TokenRequest,
+  TokenReview,
+  ValidatingWebhookConfiguration,
+  VolumeAttachment,
+}
+
 delete Models.Namespace;
-delete Models.Pod;
-delete Models.Endpoints;
 
 class Namespace extends K8Object {
   constructor(config) {
@@ -24,21 +114,70 @@ class Namespace extends K8Object {
   static kind = 'Namespace';
   static Model = Model;
 
-  async delete () {
-    return Promise.all([
-      Pod.find({ 'metadata.namespace': this.metadata.name }),
-      Endpoints.find({ 'metadata.namespace': this.metadata.name }),
-    ])
-    .then((data) => {
-      let [ pods, endpoints ] = data;
-      pods.map((p) => p.delete());
-      endpoints.map((e) => e.delete());
-      return [
-        ...Object.keys(Models).map((m) => Models[m].findOneAndDelete({ 'metadata.namespace': this.metadata.name })),
-        Model.findOneAndDelete({ 'metadata.name': this.metadata.name }),
+  static create (config) {
+    if (!config.metadata) {
+      return Promise.reject(super.unprocessableContentStatus());
+    }
+    if (!config.metadata.name) {
+      config.metadata.name = 'default'
+    }
+    if (!config.metadata.labels) {
+      config.metadata.labels = new Map([['name', config.metadata.name]]);
+    }
+    return super.findOne({ 'metadata.name': config.metadata.name })
+    .then((existingNamespace) => {
+      if (existingNamespace) {
+        throw super.alreadyExistsStatus(config.metadata.name);
+      }
+      let genCert = (attrs) => selfsigned.generate(attrs, { keySize: 2048, days: 3650, extensions: [{ name: 'subjectAltName', altNames: [] }] }).cert.trim().replaceAll('\r', '');
+      let certs = [
+        genCert([{ name: 'commonName', value: 'kube-apiserver-lb-signer' }]),
+        genCert([{ name: 'commonName', value: 'kube-apiserver-localhost-signer' }]),
+        genCert([{ name: 'commonName', value: 'kube-apiserver-service-network-signer' }]),
       ];
+      return Promise.all([
+        ServiceAccount.create({
+          metadata: {
+            name: "builder",
+            namespace: config.metadata.name,
+          }
+        }),
+        ServiceAccount.create({
+          metadata: {
+            name: "default",
+            namespace: config.metadata.name,
+          }
+        }),
+        ServiceAccount.create({
+          metadata: {
+            name: "deployer",
+            namespace: config.metadata.name,
+          }
+        }),
+        ConfigMap.create({
+          metadata: {
+            name: 'kube-root-ca.crt',
+            namespace: config.metadata.name,
+          },
+          data: new Map()
+        })
+        .then((configMap) => {
+          // configMap.data.set('ca.crt', certs.join('\n'));
+          return configMap.update({ data: configMap.data });
+        })
+      ])
+      .then(() => new Model(config).save());
     })
-    .then((arr) => Promise.all(arr))
+  }
+
+  async delete () {
+    return Promise.all(Object.keys(Models).map((m) => Models[m].find({ 'metadata.namespace': this.metadata.name })))
+    .then((resources) => {
+      return Promise.all([
+        ...resources.flat().map((resource) => new objects[resource.kind](resource).delete()),
+        super.delete({ 'metadata.name': this.metadata.name }),
+      ]);
+    })
     .then((promises) => {
       let namespace = promises.at(-1);
       if (namespace) {
