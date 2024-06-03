db74d3fccb951b13dd137f89d79e52203b2020f6
Seth Wheeler <23089578+Megapixel99@users.noreply.github.com>
2024-06-03 09:43:17 -0500

    use middleware of delete all route

--- routes/all.js
+++ routes/all.js
@@ -2,12 +2,8 @@ const router = require('express').Router();
 const objects = require('../objects');
 const { general } = require('../middleware');
 
-router.delete('/all', (req, res, next) => {
-  Promise.all(Object.keys(objects).map((obj) => {
-    return general.delete(objects[obj], false)(req, res, next);
-  }))
-  .then(() => res.sendStatus(200))
-  .catch(next);
+router.delete('/all', ...Object.values(objects).map((obj) => general.delete(obj, false)), (req, res, next) => {
+  res.sendStatus(200)
 });
 
 module.exports = router;
